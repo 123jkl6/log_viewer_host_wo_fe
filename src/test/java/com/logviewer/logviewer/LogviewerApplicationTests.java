699db74c779834f75a39a061f4bc3274fc443ff5
developer <nicholasngchuxu@gmail.com>
2019-12-06 00:04:40 +0800

    Skeleton springboot

---
+++ src/test/java/com/logviewer/logviewer/LogviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logviewer.logviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
